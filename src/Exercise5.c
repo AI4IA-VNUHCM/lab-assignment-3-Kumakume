ec74c89346b24b81e8d171abbca6619cb29f17f1
Kumakume <ITITIU20165@student.hcmiu.edu.vn>
2021-10-18 16:41:09 +0700

    new

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -15,24 +15,15 @@ ______________________________________
 void Ex5(int arr[], int n){
 	//Your codes here
 	int temp[n]; 
-  
-    // Traversal array and store +ve element in 
-    // temp array 
-    int j = 0; // index of temp 
+    int j = 0;
     for (int i = 0; i < n ; i++) 
         if (arr[i] >= 0 ) 
             temp[j++] = arr[i]; 
-  
-    // If array contains all positive or all negative. 
     if (j == n || j == 0) 
         return; 
-  
-    // Store -ve element in temp array 
     for (int i = 0 ; i < n ; i++) 
         if (arr[i] < 0) 
             temp[j++] = arr[i]; 
-  
-    // Copy contents of temp[] to arr[] 
     memcpy(arr, temp, sizeof(temp));
 	for (int i = 0; i < n; i++) 
        printf("%d ",arr[i]);
