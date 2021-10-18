fa936e1de6fa308f98896207a4218d20f6d4b8a6
Kumakume <ITITIU20165@student.hcmiu.edu.vn>
2021-10-18 15:56:00 +0700

    up

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -14,7 +14,23 @@ ______________________________________
 
 int Ex4(int arr[], int n){
 	//Your codes here
-	
+	/*int i;
+    for(i = 0; i < n - 1; i ++);
+	{
+        if (arr[i] < arr[i+1]) 
+		{
+            return 1;
+        } 
+		else
+		{
+            return -1;   
+        }
+	}*/
+	if (n == 1 || n == 0) 
+        return 1; 
+    if (arr[n - 1] < arr[n - 2]) 
+        return -1; 
+    return Ex4(arr, n - 1);
 }
 
 int main(int argc, char *argv[]) {
